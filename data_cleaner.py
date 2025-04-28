3c8d90400d9331e81ed5ba612acdb22cc20fcac5
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-04-28 15:27:10 +0000

    update 4/28

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import numpy as np
+
+data = pd.read_csv("world_population.csv")
+
+np.random.seed(0)
+data.head()
+missing_values_count = data.isnull().sum()
+missing_values_count[0:10]
+
+total_cells = np.product(data.shape)
+total_missing = missing_values_count.sum()
+
+# percent of data that is missing
+percent_missing = (total_missing/total_cells) * 100
+print(percent_missing)
--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 def main():
-    try:
-        data = []
-        with open("world_population.csv", 'r') as f:
-            lines = f.readlines()
+    pass
