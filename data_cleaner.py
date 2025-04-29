82a76a99187357503302910e281265f7dd32ddf5
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-04-29 15:26:59 +0000

    update 4/29

--- data_cleaner.py
+++ data_cleaner.py
@@ -11,6 +11,19 @@ missing_values_count[0:10]
 total_cells = np.product(data.shape)
 total_missing = missing_values_count.sum()
 
-# percent of data that is missing
 percent_missing = (total_missing/total_cells) * 100
 print(percent_missing)
+missing_values_count[0:10]
+
+data.dropna()
+columns_with_na_dropped = data.dropna(axis=1)
+columns_with_na_dropped.head()
+
+print("Columns in original dataset: %d \n" % data.shape[1])
+print("Columns with na's dropped: %d" % columns_with_na_dropped.shape[1])
+
+subset_data = data.loc[:, 'Rank':'Capital'].head()
+subset_data
+
+subset_data.fillna(0)
+subset_data.fillna(method='bfill', axis=0).fillna(0)
