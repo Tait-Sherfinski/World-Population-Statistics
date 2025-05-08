08f7d6059fe5dfba74c0327de48540c3f5ab6f72
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-05-08 15:26:18 +0000

    update 5/8

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-def main():
-    pass
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('world_population.csv')
+print(data.head(10))
+
+plt.figure(figsize=(12,8))
+data.head(500).boxplot(column='Growth Rate', by='Continent')
+plt.suptitle('')
+plt.title('Country growth rates by continent')
+plt.xlabel('Continent')
+plt.ylabel('Growth rate')
+plt.xticks(rotation=90)
+plt.show()
